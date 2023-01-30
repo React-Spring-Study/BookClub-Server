a3a08b7ba8c1cb9edcdfb0018c35bcf208274969
NamJihyun99 <namjihyun9936@gmail.com>
2023-01-31 04:19:03 +0900

    feat:> comment entity/#1

---
+++ bookclub/src/main/java/traveler/bookclub/entity/Comment.java
@@ -0,0 +1,25 @@
+package traveler.bookclub.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(name = "comment_content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
