7ec8297197d2fe97fd6a7b44b62c39f12f92184a
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-05 17:40:00 +0900

    feat:> common api response/#3

---
+++ bookclub/src/main/java/traveler/bookclub/common/ErrorEntity.java
@@ -0,0 +1,22 @@
+package traveler.bookclub.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorEntity {
+
+    private String errorCode;
+    private String errorMessage;
+    private Map<String, String> errors;
+
+    public ErrorEntity(String errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
