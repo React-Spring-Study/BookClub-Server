ca71c5eef4ead20cf1cba92d537bc680ea8f773f
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-06 14:04:48 +0900

    feat:> ClubRepository/#5

---
+++ bookclub/src/main/java/traveler/bookclub/club/ClubRepository.java
@@ -0,0 +1,9 @@
+package traveler.bookclub.club;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import traveler.bookclub.entity.Member;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+
+    <Optional>Club findAllByHost(Member member);
+}
