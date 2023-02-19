b7d1be4ae95ec140095dac7490f629113646203c
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-19 16:02:16 +0900

    feat:>base time entity localdatetime format/#14

--- bookclub/src/main/java/traveler/bookclub/entity/BaseTimeEntity.java
+++ bookclub/src/main/java/traveler/bookclub/entity/BaseTimeEntity.java
@@ -1,24 +1,34 @@
 package traveler.bookclub.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
-@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createdDate;
+    private String createdDate;
 
     @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    private String modifiedDate;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.modifiedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 }
