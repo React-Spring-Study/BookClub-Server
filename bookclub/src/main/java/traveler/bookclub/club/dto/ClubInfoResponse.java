719781820c9b3840644bdce8e6f765b1931ab8f5
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-19 19:20:54 +0900

    feat:> club information read/#14

--- bookclub/src/main/java/traveler/bookclub/club/dto/ClubInfoResponse.java
+++ bookclub/src/main/java/traveler/bookclub/club/dto/ClubInfoResponse.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import traveler.bookclub.club.domain.Club;
+import traveler.bookclub.clubMember.ClubMember;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -16,6 +20,7 @@ public class ClubInfoResponse {
     private String imgUrl;
     private Integer max;
     private Integer num;
+    private List<String> members;
 
     public ClubInfoResponse(String name, String imgUrl) {
         this.name = name;
@@ -28,7 +33,16 @@ public class ClubInfoResponse {
                 club.getHost().getNickname(),
                 club.getImgUrl(),
                 club.getMax(),
-                club.getNum()
+                club.getNum(),
+                toNameList(club.getMembers())
         );
+    }
+
+    private static List<String> toNameList(List<ClubMember> list) {
+        List<String> names = new ArrayList<>();
+        for (ClubMember clubMember : list) {
+            names.add(clubMember.getMember().getNickname());
+        }
+        return names;
     }
 }
